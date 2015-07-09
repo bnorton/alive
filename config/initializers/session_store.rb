b86691a8588560a4a8e00df9c619a3363d27e4e0
Brian Norton <brian.nort@gmail.com>
2015-07-08 23:18:50 -0700

    Remove the (unneded) session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,4 @@
-options = { :key => '_alive_session' }
-
 # options = { :key => '_alive_{{username}}_session' }
 # options[:domain] = '{{username}}.com' if Rails.env.production?
 
-Rails.application.config.session_store :cookie_store, options
+# Rails.application.config.session_store :cookie_store, options
